2bf4f3421694ee1dfa3f534a1c4527bf038ea3f7
Yasser Elgammal <yasser.elgammal@ymail.com>
2026-09-17 00:51:05 +0300

    feat: add ObserverRegistry singleton to DatabaseServiceProvider and create tests for observer resolution

--- src/Providers/DatabaseServiceProvider.php
+++ src/Providers/DatabaseServiceProvider.php
@@ -5,6 +5,7 @@ namespace YasserElgammal\Green\Providers;
 use YasserElgammal\Green\Support\ServiceProvider;
 use YasserElgammal\Green\Database\ConnectionPool;
 use YasserElgammal\Green\Database\Database;
+use YasserElgammal\Green\Database\ObserverRegistry;
 use YasserElgammal\Green\Config\Typed\DatabaseConfig;
 
 class DatabaseServiceProvider extends ServiceProvider
@@ -14,6 +15,13 @@ class DatabaseServiceProvider extends ServiceProvider
         $this->app->singleton(ConnectionPool::class, function ($app) {
             return new ConnectionPool($app->make(DatabaseConfig::class)->toArray());
         });
+
+        $this->app->singleton(
+            ObserverRegistry::class,
+            fn ($app) => new ObserverRegistry(
+                fn (string $observerClass) => $app->make($observerClass),
+            ),
+        );
     }
 
     public function boot(): void
